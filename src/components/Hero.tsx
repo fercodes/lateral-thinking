a4570349e161e8c958e7a5f525cb5773f71d9002
Fernando <59489587+fercodes@users.noreply.github.com>
2025-05-17 11:45:36 -0400

    Add background gradient animation

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import { BackgroundGradientAnimation } from '@/components/ui/background-gradient-animation';
+
+const Hero = () => {
+  return (
+    <BackgroundGradientAnimation>
+      <h1 className="text-white text-4xl font-bold">Welcome to my site!</h1>
+    </BackgroundGradientAnimation>
+  );
+};
+
+export default Hero;
